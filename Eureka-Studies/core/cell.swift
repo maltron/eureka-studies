ce3df1793db6e00a60adc9fb66ac29bb9a5d816a
Mauricio Leal <maltron@gmail.com>
2018-01-06 09:17:02 -0200

    Created a simple cell

---
+++ Eureka-Studies/core/cell.swift
@@ -0,0 +1,15 @@
+//
+//  cell.swift
+//  Eureka-Studies
+//
+//  Created by Mauricio Leal on 1/5/18.
+//  Copyright © 2018 Mauricio Leal. All rights reserved.
+//
+
+import Foundation
+
+public protocol CellType: class {
+    
+}
+
+
